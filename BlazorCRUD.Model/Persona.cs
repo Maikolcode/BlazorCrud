3898e72cef891a6e9b4d4ca12c6f0a30697990ab
MaikolCode <maikol320434@gmail.com>
2021-10-02 10:07:18 -0500

    Se crea la conexion a base de datos, ademas se referencias los servicios nuevos

---
+++ BlazorCRUD.Model/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Model
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Correo { get; set; }
+        public string Numero { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
